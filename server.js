de91a3df4c7c5b009ace8efa9bd0e890eb98e703
stefian <v_ianta@yahoo.com>
2021-01-24 05:33:20 -0400

    added basic spreadsheets routes

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/sheets', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all sheets' });
+});
+
+app.get('/api/v1/sheets/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Show spreadsheet ${req.params.id}` });
+});
+
+app.post('/api/v1/sheets', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new spreadsheet' });
+});
+
+app.put('/api/v1/sheets/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update spreadsheet ${req.params.id}` });
+});
+
+app.delete('/api/v1/sheets/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete spreadsheet ${req.params.id}` });
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
