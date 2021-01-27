e55f0e9328ddd173e9d7f14c65ab9ced504c79e1
stefian <v_ianta@yahoo.com>
2021-01-27 14:55:24 -0400

    +     required: [true, 'Please add a description'],

--- models/Spreadsheet.js
+++ models/Spreadsheet.js
@@ -17,7 +17,7 @@ const SpreadsheetSchema = new mongoose.Schema({
   slug: String,
   description: {
     type: String,
-    required: [true, 'Please add a description < 500 characters'],
+    required: [true, 'Please add a description'],
     maxlength: [500, 'Description can not be more than 500 characters']
   },
   website: {
