18f08571097a2e1ca5db081c9b1eb073e594f1f0
stefian <v_ianta@yahoo.com>
2021-01-28 07:02:14 -0400

    + // Geocode & create location field - using geocoder Promise format SpreadsheetSchema.pre('save', async function (next) {   const loc = await geocoder.geocode(this.address);   this.location = {     type: 'Point',     coordinates: [loc[0].longitude, loc[0].latitude],     formattedAddress: loc[0].formattedAddress,     street: loc[0].streetName,     city: loc[0].city,     state: loc[0].stateCode,     zipcode: loc[0].zipcode,     country: loc[0].countryCode   }
      // Do not save address in DB
      this.address = undefined;
      next();
    });

--- models/Spreadsheet.js
+++ models/Spreadsheet.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const SpreadsheetSchema = new mongoose.Schema({
   name: {
@@ -109,6 +110,25 @@ const SpreadsheetSchema = new mongoose.Schema({
 // Create spreadsheet slug from the name
 SpreadsheetSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true})
+  next();
+});
+
+// Geocode & create location field - using geocoder Promise format
+SpreadsheetSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  }
+
+  // Do not save address in DB
+  this.address = undefined;
   next();
 });
 
