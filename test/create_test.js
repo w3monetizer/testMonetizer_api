a757d035a5c6a5386540b79640487fcda312a093
stefian <v_ianta@yahoo.com>
2021-01-30 19:38:29 -0400

    + describe('Creating records', () => {   it('saves a user', () => {     const joe = new User({ name: 'Joe' });  // new user in memory //   });
      joe.save();
    });

--- test/create_test.js
+++ test/create_test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
+const User = require('../src/user');
 
 describe('Creating records', () => {
   it('saves a user', () => {
-    assert(1 + 1 === 2)
-  })
+    const joe = new User({ name: 'Joe' });  // new user in memory //
+  });
+
+  joe.save();
 });
