4d6ceb5bbb5555299e1ebb812d5ec50637c58499
stefian <v_ianta@yahoo.com>
2021-01-31 12:40:04 -0400

    +   // Pagination   const page = parseInt(req.query.page, 10) || 1;   const limit = parseInt(req.query.limit, 10) || 1;   const skip = (page - 1) * limit;
      query = query.skip(skip).limit(limit);

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -15,7 +15,7 @@ exports.getJobs = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page','limit'];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
@@ -42,6 +42,13 @@ exports.getJobs = asyncHandler(async (req, res, next) => {
   } else {
     query = query.sort('-createdAt'); // Descending sort by date //
   }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const skip = (page - 1) * limit;
+
+  query = query.skip(skip).limit(limit);
 
   // Executing query
   const jobs = await query;
