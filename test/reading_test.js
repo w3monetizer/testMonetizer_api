1adc0f3f751aca16b9f36dce9973fa7c93be6d6a
stefian <v_ianta@yahoo.com>
2021-02-01 11:07:30 -0400

    +     "test": "nodemon --exec 'mocha -R min --timeout 3000 --exit'"

--- test/reading_test.js
+++ test/reading_test.js
@@ -27,4 +27,12 @@ describe('Reading users out of the database', () => {
       });
   });
 
+  it('find a user with a particular id', (done) => {
+    User.findOne({ _id: joe._id })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        done();
+      });
+  });
+
 });
