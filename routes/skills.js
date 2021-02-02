a11f74f5c53e436600d28096fbee3600c4ba16fa
stefian <v_ianta@yahoo.com>
2021-02-02 09:09:43 -0400

    + router.route('/:id').get(getSkill);

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {
-  getSkills
+  getSkills,
+  getSkill
 } = require('../controllers/skills');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getSkills);
+router.route('/:id').get(getSkill);
 
 module.exports = router;
