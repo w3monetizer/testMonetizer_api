0489ae086a398c00d03224c610d74ccca1cce13b
stefian <v_ianta@yahoo.com>
2021-02-04 19:03:30 -0400

    +   it('A user can have their postcount incremented by 1', () => {     User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })       .then(() => User.findOne({ name: 'Joe' }))       .then((user) => {         assert(user.postCount === 1);         done();       });   })

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', likes: 0 });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -59,5 +59,14 @@ describe('Updating records', () => {
   //       done();
   //     });
   // });
+
+  it('A user can have their postcount incremented by 1', () => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
+  })
 
 });
