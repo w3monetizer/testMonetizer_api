31d18d9983e1db91ca276042801d51d130e8b5ba
stefian <v_ianta@yahoo.com>
2021-02-04 09:10:37 -0400

    + const mongoose = require('mongoose');
    const SheetSchema = new mongoose.Schema({
      name: {
        type: String,
        required: [true, 'Please add a name'],
        unique: true,
        trim: true,
        maxlength: [20, 'Name can not be more than 20 characters']
      },
      rows: [[{ type: String }]],
      createdAt: {
        type: Date,
        default: Date.now
      },
    });
    
    module.exports = mongoose.model('Sheet', SheetSchema);

---
+++ models/Sheet.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const SheetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [20, 'Name can not be more than 20 characters']
+  },
+  rows: [[{ type: String }]],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+module.exports = mongoose.model('Sheet', SheetSchema);
