af12efcd3ff7336e77491766bb3c899af52df009
stefian <v_ianta@yahoo.com>
2021-02-05 09:54:55 -0400

    +   it('can create a subdocument', (done) => {     const joe = new User({       name: 'Joe',       posts: [{         title: 'PostTitle',         sheet: [           ['A', 'B', 'C'],           ['1 + 1', '=', '?']         ]       }]     });
        joe.save()
          .then(() => User.findOne({ name: 'Joe' }))
          .then((user) => {
            assert(user.posts[0].title === 'PostTitle');
            console.log(eval(user.posts[0].sheet[1][0]));
            assert(user.posts[0].sheet[1][0] === '1 + 1');
            done();
          });
      });

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -6,13 +6,21 @@ describe('Subdocuments', () => {
   it('can create a subdocument', (done) => {
     const joe = new User({
       name: 'Joe',
-      posts: [{ title: 'PostTitle' }]
+      posts: [{
+        title: 'PostTitle',
+        sheet: [
+          ['A', 'B', 'C'],
+          ['1 + 1', '=', '?']
+        ]
+      }]
     });
 
     joe.save()
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
         assert(user.posts[0].title === 'PostTitle');
+        console.log(eval(user.posts[0].sheet[1][0]));
+        assert(user.posts[0].sheet[1][0] === '1 + 1');
         done();
       });
   });
