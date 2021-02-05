c2c87a3c1c80767a883a0160732b96151acba4d9
stefian <v_ianta@yahoo.com>
2021-02-04 20:22:50 -0400

    +   name: {     type: String,     required: [true, 'Name is required.']   },

---
+++ test/validation_test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+  // Create records with Mongoose - not saved in DB //
+  it('requires a user name', () => {
+
+  });
+});
