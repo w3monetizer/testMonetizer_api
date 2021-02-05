c0557f9b86e1d8038a116aa5e38a974301dd3007
stefian <v_ianta@yahoo.com>
2021-02-05 06:31:44 -0400

    +   it('disallows invalid records from being saved', (done) => {     const user = new User({ name: 'Al' });     user.save()       .catch((validationResult) => {         const { message } = validationResult.errors.name;
            assert(message === 'Name must be longer than 2 characters.');
            done();
          });
      });

--- test/validation_test.js
+++ test/validation_test.js
@@ -28,4 +28,15 @@ describe('Validating records', () => {
 
     assert(message === 'Name must be longer than 2 characters.');
   });
+
+  it('disallows invalid records from being saved', (done) => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch((validationResult) => {
+        const { message } = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
+  });
 });
