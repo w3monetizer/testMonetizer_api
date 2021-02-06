7352464ccfbc2429920055bd7cc2513a54a46668
stefian <v_ianta@yahoo.com>
2021-02-06 13:37:20 -0400

    + solution.createNewBlock(238, 'KJYGKJYGKYGKUYFGK', 'JHFKYFJTDKUGY'); solution.createNewBlock(111, 'KJYJTYDUJYGKUYFGK', 'JHJBYTFYDKUGY'); solution.createNewBlock(278, 'KJYGMNHVNHVKUYFGK', 'WASGFHJTDKUGY');

--- dev/test.js
+++ dev/test.js
@@ -3,5 +3,7 @@ const Blockchain = require('./blockchain');
 const solution = new Blockchain();
 
 solution.createNewBlock(238, 'KJYGKJYGKYGKUYFGK', 'JHFKYFJTDKUGY');
+solution.createNewBlock(111, 'KJYJTYDUJYGKUYFGK', 'JHJBYTFYDKUGY');
+solution.createNewBlock(278, 'KJYGMNHVNHVKUYFGK', 'WASGFHJTDKUGY');
 
 console.log(solution);
