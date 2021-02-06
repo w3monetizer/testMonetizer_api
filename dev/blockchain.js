d26c56619e2d8a08703825ada5c215f49f603453
stefian <v_ianta@yahoo.com>
2021-02-06 13:10:55 -0400

    + Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {   const newBlock = {     index: this.chain.lenght + 1,     timestamp: Date.now(),     transactions: this.newTransactions,     nonce: nonce, // Proof of work - a number - proof that we created the block in a legitimate way     hash: hash, // data from our new block - all new tx compressed in a single string     previousBlockHash: previousBlockHash  // the data hashed of the previous block   };
      this.newTransactions = [];
      this.chain.push(newBlock);
    
      return newBlock;
    }

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -4,5 +4,19 @@ function Blockchain(repo) {
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
-  
+  const newBlock = {
+    index: this.chain.lenght + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce, // Proof of work - a number - proof that we created the block in a legitimate way
+    hash: hash, // data from our new block - all new tx compressed in a single string
+    previousBlockHash: previousBlockHash  // the data hashed of the previous block
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
 }
+
+module.exports = Blockchain;
