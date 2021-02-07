b872cf18826d0f3af4a512645bfedb991beeb7f1
stefian <v_ianta@yahoo.com>
2021-02-07 15:27:07 -0400

    + // Find the correct nonce where the hash starts with TRUST_STRING: 0 - 0000 ...

--- dev/test.js
+++ dev/test.js
@@ -20,10 +20,10 @@ const currentBlockData = [
   },
 ];
 
-// Find the correct nounce where the hash starts with 0000
+// Find the correct nonce where the hash starts with TRUST_STRING: 0 - 0000 ...
 console.log(solution.proofOfWork(previousBlockHash, currentBlockData));
 
-// Check that current solution hash starts with 1-4 zeros
+// Check that current solution hash starts with 1-4 zeros / TRUST_STRING
 console.log(0, solution.hashBlock(previousBlockHash, currentBlockData, 0)); // 0
 console.log(304, solution.hashBlock(previousBlockHash, currentBlockData, 304)); // 00
 console.log(9986, solution.hashBlock(previousBlockHash, currentBlockData, 9986)); // 000
