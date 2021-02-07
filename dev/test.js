9f2e35fd05bcdff724ab549e973b7db7ecf86089
stefian <v_ianta@yahoo.com>
2021-02-06 22:36:46 -0400

    + const solution = new Blockchain();

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,6 @@ const Blockchain = require('./blockchain');
 
 const solution = new Blockchain();
 
-solution.createNewBlock(238, 'KJYGKJYGKYGKUYFGK', 'JHFKYFJTDKUGY');
-solution.createNewBlock(111, 'KJYJTYDUJYGKUYFGK', 'JHJBYTFYDKUGY');
-solution.createNewBlock(278, 'KJYGMNHVNHVKUYFGK', 'WASGFHJTDKUGY');
+
 
 console.log(solution);
