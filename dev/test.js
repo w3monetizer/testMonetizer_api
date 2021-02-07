660b09176f34c3507afdb1189b5e28a8dfbbe082
stefian <v_ianta@yahoo.com>
2021-02-06 23:04:14 -0400

    + solution.createNewBlock(1326987, 'JHFKVKKYFKHKG', 'KJHBKJHVKHLIKYGK');

--- dev/test.js
+++ dev/test.js
@@ -6,4 +6,6 @@ solution.createNewBlock(6548654, 'KFGKHFKYFKHKG', 'HKUGLUIHLIKYGK');
 
 solution.createNewTransaction(100, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
 
+solution.createNewBlock(1326987, 'JHFKVKKYFKHKG', 'KJHBKJHVKHLIKYGK');
+
 console.log(solution);
