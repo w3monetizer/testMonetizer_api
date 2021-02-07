0688b33adb63522514cc0fefbb2b5f4af7f5c5b8
stefian <v_ianta@yahoo.com>
2021-02-06 23:12:53 -0400

    + solution.createNewTransaction(50, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR'); solution.createNewTransaction(300, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR'); solution.createNewTransaction(2000, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');

--- dev/test.js
+++ dev/test.js
@@ -8,4 +8,8 @@ solution.createNewTransaction(100, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
 
 solution.createNewBlock(1326987, 'JHFKVKKYFKHKG', 'KJHBKJHVKHLIKYGK');
 
+solution.createNewTransaction(50, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
+solution.createNewTransaction(300, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
+solution.createNewTransaction(2000, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
+
 console.log(solution.chain[1]);
