006c98fd17d2b499e452b5717f3ac673f578fac7
stefian <v_ianta@yahoo.com>
2021-02-06 22:43:51 -0400

    + solution.createNewTransaction(100, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,8 @@ const Blockchain = require('./blockchain');
 
 const solution = new Blockchain();
 
+solution.createNewBlock(6548654, 'KFGKHFKYFKHKG', 'HKUGLUIHLIKYGK');
 
+solution.createNewTransaction(100, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
 
 console.log(solution);
