ab4f67262c9dc638c45d253625a9868fa0133f23
stefian <v_ianta@yahoo.com>
2021-02-07 09:06:25 -0400

    + console.log(solution.hashBlock(previousBlockHash, currentBlockData, nonce));

--- dev/test.js
+++ dev/test.js
@@ -1,3 +1,25 @@
 const Blockchain = require('./blockchain');
 const solution = new Blockchain();
 
+const previousBlockHash = 'HKTYKVKVYTCJYTCJYTCVJRDJC';
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: 'HGDRVHGFBJHFJF',
+    recipient: 'FSDVHGGKUGKFJYT'
+  },
+  {
+    amount: 30,
+    sender: 'KJGKJVKUHGFBJHFJF',
+    recipient: 'JHDHDVDTKUGKFJYT'
+  },
+  {
+    amount: 200,
+    sender: 'JHFJHGJYFTJYDJ',
+    recipient: 'HGRDFJFKFGJDD'
+  },
+];
+const nonce = 100;
+
+
+console.log(solution.hashBlock(previousBlockHash, currentBlockData, nonce));
