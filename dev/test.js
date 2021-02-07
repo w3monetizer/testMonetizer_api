a9e98488618291f41a4244f58eecae70f3fd54f5
stefian <v_ianta@yahoo.com>
2021-02-07 08:49:30 -0400

    + const solution = new Blockchain();

--- dev/test.js
+++ dev/test.js
@@ -1,17 +1,3 @@
 const Blockchain = require('./blockchain');
-
 const solution = new Blockchain();
 
-solution.createNewBlock(6548654, 'KFGKHFKYFKHKG', 'HKUGLUIHLIKYGK');
-
-solution.createNewTransaction(100, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
-
-solution.createNewBlock(1326987, 'JHFKVKKYFKHKG', 'KJHBKJHVKHLIKYGK');
-
-solution.createNewTransaction(50, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
-solution.createNewTransaction(300, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
-solution.createNewTransaction(2000, 'ALEX-SENDER-ADDR', 'JENN-RECIPIENT-ADDR');
-
-solution.createNewBlock(8353487, 'KHGFITFKYFKHKG', 'FWEQFEHVKHLIKYGK');
-
-console.log(solution.chain[2]);
