c5f384085d94c6fc6d9bf9f1f0324c7175a35e97
stefian <v_ianta@yahoo.com>
2021-02-08 14:46:59 -0400

    + app.listen(port, function () {   console.log(`Listening on port ${port}...`); });

--- dev/webNode.js
+++ dev/webNode.js
@@ -57,6 +57,6 @@ app.get('/test', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...');
+app.listen(port, function () {
+  console.log(`Listening on port ${port}...`);
 });
