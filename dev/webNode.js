a54531fe76fc693b38e01471929b4e8ccf35bf01
stefian <v_ianta@yahoo.com>
2021-02-09 02:55:50 -0400

    + // register a node with the network app.post('/register-node', function (req, res) {   const newNodeUrl = req.body.newNodeUrl;   const nodeNotAlreadyPresent = solution.webNodes.indexOf(newNodeUrl) == -1;   const notCurrentNode = solution.currentNodeUrl !== newNodeUrl;   if(nodeNotAlreadyPresent && notCurrentNode) solution.webNodes.push(newNodeUrl);   res.json({ note: 'New node registered successfully.' }); });

--- dev/webNode.js
+++ dev/webNode.js
@@ -100,7 +100,11 @@ app.post('/register-and-broadcast-node', function (req, res) {
 
 // register a node with the network
 app.post('/register-node', function (req, res) {
-  
+  const newNodeUrl = req.body.newNodeUrl;
+  const nodeNotAlreadyPresent = solution.webNodes.indexOf(newNodeUrl) == -1;
+  const notCurrentNode = solution.currentNodeUrl !== newNodeUrl;
+  if(nodeNotAlreadyPresent && notCurrentNode) solution.webNodes.push(newNodeUrl);
+  res.json({ note: 'New node registered successfully.' });
 });
 
 
