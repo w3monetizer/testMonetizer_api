f01d286eb735f45c940abb9c9a983e36219d5a1a
stefian <v_ianta@yahoo.com>
2021-02-14 20:00:24 -0400

    + LinkedList.prototype.addToHead = function (value) {   var newNode = new Node(value, this.head, null);   if (this.head) this.head.prev = newNode;   else this.tail = newNode; // a single node is both the head and tail node   this.head = newNode; }

--- loop/linkedList.js
+++ loop/linkedList.js
@@ -11,4 +11,7 @@ function Node(value, next, prev) {
 
 LinkedList.prototype.addToHead = function (value) {
   var newNode = new Node(value, this.head, null);
+  if (this.head) this.head.prev = newNode;
+  else this.tail = newNode; // a single node is both the head and tail node
+  this.head = newNode;
 }
