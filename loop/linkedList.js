c8ee03e74779ce526610470c439e96ebc94f4c3f
stefian <v_ianta@yahoo.com>
2021-02-15 05:52:31 -0400

    + LinkedList.prototype.addToTail = function (value) {   var newNode = new Node(value, null, this.tail);   if (this.tail) this.tail.next = newNode;   else this.head = newNode;   this.tail = newNode; }

--- loop/linkedList.js
+++ loop/linkedList.js
@@ -17,5 +17,8 @@ LinkedList.prototype.addToHead = function (value) {
 }
 
 LinkedList.prototype.addToTail = function (value) {
-  
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
 }
