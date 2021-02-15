e8983ea63cf68b1fed0b90fdd22fa370252a9408
stefian <v_ianta@yahoo.com>
2021-02-15 07:09:15 -0400

    + LinkedList.prototype.removeHead = function () {   if (!this.head) return null;   var val = this.head.value;   this.head = this.head.next;   if (this.head) this.head.prev = null;   else this.tail = null;   return val; }

--- loop/linkedList.js
+++ loop/linkedList.js
@@ -24,5 +24,10 @@ LinkedList.prototype.addToTail = function (value) {
 }
 
 LinkedList.prototype.removeHead = function () {
-  
+  if (!this.head) return null;
+  var val = this.head.value;
+  this.head = this.head.next;
+  if (this.head) this.head.prev = null;
+  else this.tail = null;
+  return val;
 }
