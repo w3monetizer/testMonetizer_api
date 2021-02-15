a6cb413a3957283d72cb3de1f3e7414168edadb5
stefian <v_ianta@yahoo.com>
2021-02-15 07:21:44 -0400

    + LinkedList.prototype.removeTail = function () {   if (!this.tail) return null;   var val = this.tail.value;   this.tail = this.tail.prev;   if (this.tail) this.tail.next = null;   else this.head = null;   return val; }

--- loop/linkedList.js
+++ loop/linkedList.js
@@ -33,5 +33,10 @@ LinkedList.prototype.removeHead = function () {
 }
 
 LinkedList.prototype.removeTail = function () {
-  
+  if (!this.tail) return null;
+  var val = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) this.tail.next = null;
+  else this.head = null;
+  return val;
 }
