b5aec21eee302489d510da2e1dba0a89b726b4e1
stefian <v_ianta@yahoo.com>
2021-02-16 07:40:50 -0400

    + // BST insert method BST.prototype.insert = function (value) {   if (value <= this.value) {     if (!this.left) this.left = new BST(value);     else this.left.insert(value);   }   else if (value > this.value) {     if (!this.right) this.right = new BST(value);     else this.right.insert(value);   } };

--- loop/bst.js
+++ loop/bst.js
@@ -1,11 +1,20 @@
+// Binary Search Tree - BST constructor
 function BST(value) {
   this.value = value;
   this.left = null;
   this.right = null;
 }
 
+// BST insert method
 BST.prototype.insert = function (value) {
-  
+  if (value <= this.value) {
+    if (!this.left) this.left = new BST(value);
+    else this.left.insert(value);
+  }
+  else if (value > this.value) {
+    if (!this.right) this.right = new BST(value);
+    else this.right.insert(value);
+  }
 };
 
 let bst = new BST(50);
