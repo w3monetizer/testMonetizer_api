7a06e936f4daaced67c609a0f8ff4322588df544
stefian <v_ianta@yahoo.com>
2021-02-16 07:53:37 -0400

    + // BST Testing let bst = new BST(50);
    bst.insert(30);
    bst.insert(70);
    bst.insert(100);
    bst.insert(60);
    bst.insert(59);
    bst.insert(20);
    bst.insert(45);
    bst.insert(35);
    bst.insert(85);
    bst.insert(105);
    bst.insert(10);
    
    console.log(bst);

--- loop/bst.js
+++ loop/bst.js
@@ -17,4 +17,19 @@ BST.prototype.insert = function (value) {
   }
 };
 
+// BST Testing
 let bst = new BST(50);
+
+bst.insert(30);
+bst.insert(70);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(20);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
+
+console.log(bst);
