44efaec035ba5cac96cae90e7bc7d70717538752
stefian <v_ianta@yahoo.com>
2021-02-17 08:14:24 -0400

    + // BST Testing // let bst = new BST(50);

--- loop/bst.js
+++ loop/bst.js
@@ -88,23 +88,23 @@ BST.prototype.delete = function (value) {
 }
 
 // BST Testing
-let bst = new BST(50);
+// let bst = new BST(50);
 
-bst.insert(30);
-bst.insert(70);
-bst.insert(100);
-bst.insert(60);
-bst.insert(59);
-bst.insert(20);
-bst.insert(45);
-bst.insert(35);
-bst.insert(85);
-bst.insert(105);
-bst.insert(10);
+// bst.insert(30);
+// bst.insert(70);
+// bst.insert(100);
+// bst.insert(60);
+// bst.insert(59);
+// bst.insert(20);
+// bst.insert(45);
+// bst.insert(35);
+// bst.insert(85);
+// bst.insert(105);
+// bst.insert(10);
 
-// Testing delete
-bst.delete(106);
-bst.depthFirstTraversal(log, 'in-order');
+// Testing delete()
+// bst.delete(106);
+// bst.depthFirstTraversal(log, 'in-order');
 
 // Testing getMaxVal()
 // console.log( bst.getMaxVal());
