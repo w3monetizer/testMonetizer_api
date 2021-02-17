80fe0a3e4f25fa22a644c55414092286327191d7
stefian <v_ianta@yahoo.com>
2021-02-17 10:57:34 -0400

    + // Hash method for hashing the key string into a number HashTable.prototype.hash = function (key) {   // Add all unicode values for the chars in the key string into a large number   let total = 0;   for (let i = 0; i < key.length; i++) {     total += key.charCodeAt(i);   }   const bucket = total % this.numBuckets;   return bucket; }

--- loop/hashTable.js
+++ loop/hashTable.js
@@ -11,13 +11,22 @@ function HashNode(key, value, next) {
   this.next = next || null;
 }
 
+// Hash method for hashing the key string into a number
+HashTable.prototype.hash = function (key) {
+  // Add all unicode values for the chars in the key string into a large number
+  let total = 0;
+  for (let i = 0; i < key.length; i++) {
+    total += key.charCodeAt(i);
+  }
+  const bucket = total % this.numBuckets;
+  return bucket;
+}
 
 // Viz and Test HashTable
 let myHT = new HashTable(30);
 console.log(myHT);
 
-// Examples of 'string'.charCodeAt()
+// Examples of 'string'.charCodeAt() & modulus %
 // console.log('hello world'.charCodeAt(1));  // 101 - Unicode for 'e'
 // console.log('hello world'.charCodeAt(4));  // 111 - Unicode for 'o'
-
-console.log(100 % 30);
+// console.log(100 % 30);  // 10
